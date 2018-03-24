1ae1e1b66374f7dce1a0a50452ac1d4a4242f420
MindaugasVainauskas <g00327967@gmit.ie>
2018-03-24 14:15:31 +0000

    Added comments for problem 5

--- problem-5_hamming-weight.rkt
+++ problem-5_hamming-weight.rkt
@@ -2,16 +2,16 @@
 ;Problem 5. Calculating hamming weight of a given binary list.
 
 (define (hamming-weight l)
-  (if (null? l)
-      '()
-      (sum-elements l)))
+  (if (null? l); If list is null/empty
+      '()  ;Return resulting sum
+      (sum-elements l)))  ;Else recursively call the helper function
 
 ;Helper function for counting non 0 elements in the list
 (define (sum-elements l)
-  (cond ((null? l) 0)
-        ((not (= (car l) 0))
-        (+ 1(sum-elements (cdr l))))
-        (else (sum-elements (cdr l)))
+  (cond ((null? l) 0) ; If list is null/empty return 0.
+        ((not (= (car l) 0))  ;If current element is non zero
+        (+ 1(sum-elements (cdr l))))  ; Add 1 to the amont of non zero elements in list
+        (else (sum-elements (cdr l))) ; Otherwise recursively call the helper function
     
     ))
 
